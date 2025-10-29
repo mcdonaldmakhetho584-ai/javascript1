cdf692aee22a2cf171085a24ae501d73f43b1284
mcdonaldmakhetho584-ai <mcdonaldmakhetho584@gmail.com>
2025-10-29 15:26:47 +0200

    chicken

---
+++ Basics.js
@@ -0,0 +1,20 @@
+// TODO: Create a variable called firstName and store your first name in it
+const firstName = "Mcdonald";
+
+// TODO: Create a variable called lastName and store your last name in it
+const lastName = "Makhetho";
+
+// TODO: Create a variable called age using let (you'll modify this later)
+let age = 19;
+
+// TODO: Create a fullName variable that combines firstName and lastName
+const fullName = firstName + " " + lastName;
+
+// TODO: Print a greeting to the console using these variables
+console.log("Hello! My name is " + fullName + " and I am " + age + " years old.");
+
+// TODO: Try changing the age variable to a different value
+age = 20;
+
+// Print the updated greeting to see the change
+console.log("Next year, I’ll be " + age + " years old!");
